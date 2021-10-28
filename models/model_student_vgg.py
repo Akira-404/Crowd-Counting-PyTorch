3e5927bf2869d3e39165e7742af7ae4c987f8a9d
lee linfeng <2033211717@qq.com>
2021-10-28 16:41:13 +0800

    fixbug

--- models/model_student_vgg.py
+++ models/model_student_vgg.py
@@ -19,43 +19,44 @@ class CSRNet(nn.Module):
         self.seen = 0
         self.transform = transform
         channel = channel_nums[ratio - 2]
-        self.conv0_0 = conv_layers(3, channel[0])
+        # self.conv0_0 = conv_layers(3, channel[0])
+        self.conv0_0 = _conv_layers(3, channel[0])
         if self.transform:
-            self.transform0_0 = feature_transform(channel[0], 64)
-        self.conv0_1 = conv_layers(channel[0], channel[0])
+            self.transform0_0 = _feature_transform(channel[0], 64)
+        self.conv0_1 = _conv_layers(channel[0], channel[0])
 
         self.pool0 = pool_layers()
         if transform:
-            self.transform1_0 = feature_transform(channel[0], 64)
-        self.conv1_0 = conv_layers(channel[0], channel[1])
-        self.conv1_1 = conv_layers(channel[1], channel[1])
+            self.transform1_0 = _feature_transform(channel[0], 64)
+        self.conv1_0 = _conv_layers(channel[0], channel[1])
+        self.conv1_1 = _conv_layers(channel[1], channel[1])
 
         self.pool1 = pool_layers()
         if transform:
-            self.transform2_0 = feature_transform(channel[1], 128)
-        self.conv2_0 = conv_layers(channel[1], channel[2])
-        self.conv2_1 = conv_layers(channel[2], channel[2])
-        self.conv2_2 = conv_layers(channel[2], channel[2])
+            self.transform2_0 = _feature_transform(channel[1], 128)
+        self.conv2_0 = _conv_layers(channel[1], channel[2])
+        self.conv2_1 = _conv_layers(channel[2], channel[2])
+        self.conv2_2 = _conv_layers(channel[2], channel[2])
 
         self.pool2 = pool_layers()
         if transform:
-            self.transform3_0 = feature_transform(channel[2], 256)
-        self.conv3_0 = conv_layers(channel[2], channel[3])
-        self.conv3_1 = conv_layers(channel[3], channel[3])
-        self.conv3_2 = conv_layers(channel[3], channel[3])
+            self.transform3_0 = _feature_transform(channel[2], 256)
+        self.conv3_0 = _conv_layers(channel[2], channel[3])
+        self.conv3_1 = _conv_layers(channel[3], channel[3])
+        self.conv3_2 = _conv_layers(channel[3], channel[3])
 
-        self.conv4_0 = conv_layers(channel[3], channel[3], dilation=2)
+        self.conv4_0 = _conv_layers(channel[3], channel[3], dilation=True)
         if transform:
-            self.transform4_0 = feature_transform(channel[3], 512)
-        self.conv4_1 = conv_layers(channel[3], channel[3], dilation=2)
-        self.conv4_2 = conv_layers(channel[3], channel[3], dilation=2)
-        self.conv4_3 = conv_layers(channel[3], channel[2], dilation=2)
+            self.transform4_0 = _feature_transform(channel[3], 512)
+        self.conv4_1 = _conv_layers(channel[3], channel[3], dilation=True)
+        self.conv4_2 = _conv_layers(channel[3], channel[3], dilation=True)
+        self.conv4_3 = _conv_layers(channel[3], channel[2], dilation=True)
         if transform:
-            self.transform4_3 = feature_transform(channel[2], 256)
-        self.conv4_4 = conv_layers(channel[2], channel[1], dilation=2)
-        self.conv4_5 = conv_layers(channel[1], channel[0], dilation=2)
+            self.transform4_3 = _feature_transform(channel[2], 256)
+        self.conv4_4 = _conv_layers(channel[2], channel[1], dilation=True)
+        self.conv4_5 = _conv_layers(channel[1], channel[0], dilation=True)
 
-        self.conv5_0 = nn.Conv2d(channel[0], 1, kernel_size=1)
+        self.conv5_0 = nn.Conv2d(channel[0], 1, kernel_size=(1, 1))
 
         self._initialize_weights()
         self.features = []
@@ -120,23 +121,33 @@ class CSRNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
 
-def conv_layers(inp: int, oup: int, dilation: bool = False):
-    if dilation:
-        d_rate = 2
-    else:
-        d_rate = 1
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, kernel_size=(3, 3), padding=d_rate, dilation=(d_rate, d_rate)),
-        nn.ReLU(inplace=True)
-    )
+def _conv_layers(inp: int, oup: int, dilation: bool = False):
+    d_rate = 2 if dilation else 1
+    conv2d = nn.Conv2d(inp, oup, kernel_size=(3, 3), padding=d_rate, dilation=(d_rate, d_rate))
+    relu = nn.ReLU(inplace=True)
+    return nn.Sequential(conv2d, relu)
 
 
-def feature_transform(inp: int, oup: int):
+def _feature_transform(inp: int, oup: int):
     conv2d = nn.Conv2d(inp, oup, kernel_size=(1, 1))  # no padding
     relu = nn.ReLU(inplace=True)
-    layers = []
-    layers += [conv2d, relu]
-    return nn.Sequential(*layers)
+    return nn.Sequential(conv2d, relu)
+
+
+# conv_layers+feature_gransform
+# def _conv_layers(inp: int, oup: int, dilation: bool = False):
+#     # conv2d = nn.Conv2d(inp, oup, kernel_size=(1, 1))
+#
+#     kernel_size = (3,3)
+#     d_rate = 1
+#     padding = 0
+#     if dilation:
+#         d_rate = 2
+#         kernel_size = (3, 3)
+#         padding = d_rate
+#     conv2d = nn.Conv2d(inp, oup, kernel_size=kernel_size, padding=padding, dilation=(d_rate, d_rate))
+#     relu = nn.ReLU(inplace=True)
+#     return nn.Sequential(conv2d, relu)
 
 
 def pool_layers(ceil_mode: bool = True):
@@ -146,9 +157,15 @@ def pool_layers(ceil_mode: bool = True):
 if __name__ == '__main__':
     net = CSRNet()
     print(net)
-    with open('student_vgg.txt','w')as f:
-        f.write(str(net))
-    net_dict = net.state_dict()
-    for k, v in net_dict.items():
-        print(k, np.shape(v))
+    # with open('student_vgg.txt', 'w') as f:
+    #     f.write(str(net))
+    # net_dict = net.state_dict()
+    # for k, v in net_dict.items():
+    #     print(k, np.shape(v))
 
+    # net_conv = conv_layers(3, 5, True)
+    # print(net_conv)
+    # net_feat = feature_transform(3, 5)
+    # print(net_feat)
+    # net_ = _conv_layers(3, 5)
+    # print(net_)
