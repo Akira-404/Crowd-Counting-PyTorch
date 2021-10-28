f5c26e3f3a8940b36fa2608fea48509a7b5dc208
lee linfeng <2033211717@qq.com>
2021-10-28 11:50:37 +0800

    feat:move averagemeter class to utils from averagemeter

--- utils.py
+++ utils.py
@@ -9,6 +9,25 @@ import time
 import numpy as np
 import base64
 import cv2
+import h5py
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 def save_net(fname, net):
