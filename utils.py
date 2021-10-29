e2b942d5ff18331512799ff6e835a4c3ead17721
lee linfeng <2033211717@qq.com>
2021-10-29 09:56:12 +0800

    test

--- utils.py
+++ utils.py
@@ -145,3 +145,22 @@ def get_result(code, message, score):
     }
     print("Response data:", result)
     return jsonify(result)
+
+
+if __name__ == '__main__':
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.fc1 = nn.Linear(28 * 28, 256)
+            self.fc2 = nn.Linear(256, 64)
+            self.fc3 = nn.Linear(64, 10)
+
+        def forward(self, x):
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = F.relu(self.fc3(x))
+            return x
+
+
+    net = Net()
+    cal_para(net)
